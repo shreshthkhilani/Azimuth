b7f37e4588e527218e421cb736b576228b1a573d
Matthew <matthewbregg610@ufl.edu>
2015-06-27 02:57:37 -0400

    added zero checks

--- navigation.py
+++ navigation.py
@@ -22,13 +22,19 @@ def navigate_to_point(x, y, ship_status):
     d_x = x-ship_status["x"]
     d_y = y-ship_status["y"]
     d_magnitude = get_normal(d_x, d_y)
-    d_x_n = d_x/d_magnitude
-    d_y_n = d_y/d_magnitude
+    if(d_magnitude == 0):
+        return vector  # We are where we want to be
+    else:
+        d_x_n = d_x/d_magnitude
+        d_y_n = d_y/d_magnitude
 
     v_magnitude = get_normal(ship_status["dx"], ship_status["dy"])
-
-    v_x_n = ship_status["dx"]/v_magnitude
-    v_y_n = ship_status["dy"]/v_magnitude
+    if(v_magnitude == 0):
+        v_x_n = 0
+        v_y_n = 0
+    else:
+        v_x_n = ship_status["dx"]/v_magnitude
+        v_y_n = ship_status["dy"]/v_magnitude
 
     a_x = d_x_n - v_x_n
     a_y = d_y_n - v_y_n
@@ -36,3 +42,8 @@ def navigate_to_point(x, y, ship_status):
     vector.angle = math.atan2(a_y, a_x) + math.pi
     vector.magnitude = 1
     return vector
+
+#pi right
+#2pi left
+#2/pi down
+# 3pi/2 up
